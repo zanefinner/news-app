ecae83a64bfb5d578ed939d66f9dae582588307d
zanefinner <zanefinnergm@gmail.com>
2020-04-16 11:20:10 -0400

    Welcome view template completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,184 @@
 @include('partial.top')
-    Welcome!
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="text-center">Trending Articles</h1>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+
+
+
+        </div>
+
+
+        <div class="jumbotron">
+            <h1 class="text-center">Recent Articles</h1>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+                <h2>A Real Nice Tree</h2>
+                <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                <p class="text-muted">
+                    A short ravioli description pasta cut-off atlas for
+                    previewing preview purposes...
+                    <a href="#"><i>read on</i></a>
+                </p>
+            </div>
+
+
+
+
+        </div>
+    </div>
     @include('partial.bottom')
 </body>
 </html>
