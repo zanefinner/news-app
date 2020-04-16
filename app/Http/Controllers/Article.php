7f760cad9e5dbba3cab6e7222a0af8eb8fadc3c7
zanefinner <zanefinnergm@gmail.com>
2020-04-16 12:16:45 -0400

    created article view

--- app/Http/Controllers/Article.php
+++ app/Http/Controllers/Article.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class Article extends Controller
 {
     public function create(){
@@ -14,7 +15,16 @@ class Article extends Controller
         //search db for articles
     }
     public function present($id){
-        $data = "id is ".$id;
+
+        //use $id to retrieve data
+        #DUMMY DATA
+        $data = [
+            'title'=>'How to Write Well',
+            'content' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            'author' => 'Zane Finner',
+            'posted_at' => '12:00 GMT Time',
+            'thumbnail'=>'images/test.jpg'
+        ];
         return view('article', ['data'=>$data]);
     }
 }
--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,1 +1,54 @@
-{{ $data }}
+@include('partial.top')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-8">
+                <h1>{{$data['title']}}</h1>
+                <h3 class="text-muted">{{ $data['author']  }}</h3>
+                <img class="thumbnail" src="{{ asset($data['thumbnail']) }}">
+                <i>Image from https://google.com/</i>
+                <hr>
+                <p>{{ $data['content'] }}</p>
+                <hr>
+                <h1 class='text-muted'>More by {{ $data['author'] }}</h1>
+            </div>
+            <div class="col-12 col-md-4">
+                <h1>Related Articles</h1>
+                <hr>
+                <div class="row">
+
+                    <div class="col-12">
+                        <h2>A Real Nice Tree</h2>
+                        <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                        <p class="text-muted">
+                            A short ravioli description pasta cut-off atlas for
+                            previewing preview purposes...
+                            <a href="#"><i>read on</i></a>
+                        </p>
+                    </div>
+
+                    <div class="col-12">
+                        <h2>A Real Nice Tree</h2>
+                        <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                        <p class="text-muted">
+                            A short ravioli description pasta cut-off atlas for
+                            previewing preview purposes...
+                            <a href="#"><i>read on</i></a>
+                        </p>
+                    </div>
+
+                    <div class="col-12">
+                        <h2>A Real Nice Tree</h2>
+                        <img src="{{ asset('images/test.jpg') }}"class="thumbnail">
+                        <p class="text-muted">
+                            A short ravioli description pasta cut-off atlas for
+                            previewing preview purposes...
+                            <a href="#"><i>read on</i></a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('partial.bottom')
+    </div>
+</body>
+</html>
